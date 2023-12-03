6a61a205eb886b5129f137530cd2485522e926e0
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-12-03 19:10:12 +0900

    Section 4. 스프링 빈과 의존관계
    - 자바 코드로 직접 스프링 빈 등록하기

--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -9,7 +9,6 @@ public class MemberController {
 
     // MemberService는 여러 개를 생성할 이유가 없음
     // private final MemberService memberService = new MemberService();
-
     private final MemberService memberService;
 
     // 스프링 컨테이너에서 memberService를 가져옴
